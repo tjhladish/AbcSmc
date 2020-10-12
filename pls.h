dcdf0af20ffd8cbe42ce2bd4b1f3e1ade2eddcd5
Thomas Hladish <tjhladish@ufl.edu>
2020-10-11 23:07:32 -0400

    Whitespace/comment changes only

--- pls.h
+++ pls.h
@@ -16,13 +16,35 @@ typedef enum { KERNEL_TYPE1, KERNEL_TYPE2 } METHOD;
 typedef enum { PRESS, RMSEP } VALIDATION_OUTPUT;
 typedef enum { LOO, NEW_DATA } VALIDATION_METHOD;
 
+/*
+ *   Variable definitions from source paper:
+ *     X     : predictor variables matrix (N × K)
+ *     Y     : response variables matrix (N × M)
+ *     B_PLS : PLS regression coefficients matrix (K × M)
+ *     W     : PLS weights matrix for X (K × A)
+ *     P     : PLS loadings matrix for X (K × A)
+ *     Q     : PLS loadings matrix for Y (M × A)
+ *     R     : PLS weights matrix to compute scores T directly from original X (K × A)
+ *     T     : PLS scores matrix of X (N × A)
+ *     w_a   : a column vector of W
+ *     p_a   : a column vector of P
+ *     q_a   : a column vector of Q
+ *     r_a   : a column vector of R
+ *     t_a   : a column vector of T
+ *     K     : number of X-variables
+ *     M     : number of Y-variables
+ *     N     : number of objects
+ *     A     : number of components in PLS model
+ *     a     : integer counter for latent variable dimension
+ */
+
 class PLS_Model {
   public:
     Mat2Dc P, W, R, Q, T;
     int A;
     METHOD method;
     void initialize(int num_predictors, int num_responses, int num_components) {
-        A = num_components; 
+        A = num_components;
         P.setZero(num_predictors, num_components);
         W.setZero(num_predictors, num_components);
         R.setZero(num_predictors, num_components);
@@ -44,7 +66,7 @@ class PLS_Model {
         if (algorithm == KERNEL_TYPE2) XX = X.transpose() * X;
 
         for (int i=0; i<A; i++) {
-            Colc w, p, q, r, t; 
+            Colc w, p, q, r, t;
             complex<float_type> tt;
             if (M==1) {
                 w = XY.cast<complex<float_type> >();
@@ -76,17 +98,17 @@ class PLS_Model {
             R.col(i)=r;
             if (algorithm == KERNEL_TYPE1) T.col(i) = t;
         }
-        return; 
+        return;
     }
 
 
     // latent X values, i.e. the orthogonal metrics you wish you could measure
     const Mat2Dc scores(Mat2D X_new) { return scores(X_new, A); }
-    const Mat2Dc scores(Mat2D X_new, int comp) { 
+    const Mat2Dc scores(Mat2D X_new, int comp) {
         assert (A >= comp);
         return X_new * R.leftCols(comp);
     }
- 
+
     // compute the regression coefficients (aka 'beta')
     const Mat2Dc coefficients() { return coefficients(A); }
     const Mat2Dc coefficients(int comp) {
@@ -107,7 +129,7 @@ class PLS_Model {
         assert (A >= comp);
         return Y - fitted_values(X, comp);
     }
-    
+
     // Sum of squared errors
     Row SSE(const Mat2D& X, const Mat2D& Y) { return this->SSE(X, Y, A); }
     Row SSE(const Mat2D& X, const Mat2D& Y, int comp) {
@@ -115,7 +137,7 @@ class PLS_Model {
     }
 
     // Total sum of squares
-    Row SST(const Mat2D& Y) { 
+    Row SST(const Mat2D& Y) {
         Row sst(Y.cols());
         for (int c = 0; c < Y.cols(); c++) {
             sst(c) = (Y.col(c).array() - (Y.col(c).sum()/Y.rows())).square().sum();
@@ -128,14 +150,14 @@ class PLS_Model {
     Row explained_variance(const Mat2D& X, const Mat2D& Y, int comp) {
         assert (A >= comp);
     //    cerr << "ev: " << this->SSE(X, Y, comp).cwiseQuotient( SST(Y) ) << endl;
-        return (1.0 - this->SSE(X, Y, comp).cwiseQuotient( SST(Y) ).array()).matrix(); 
+        return (1.0 - this->SSE(X, Y, comp).cwiseQuotient( SST(Y) ).array()).matrix();
     }
 
     // leave-one-out validation of model (i.e., are we overfitting?)
-    Mat2D loo_validation(const Mat2D& X, const Mat2D& Y, VALIDATION_OUTPUT out_type) { 
+    Mat2D loo_validation(const Mat2D& X, const Mat2D& Y, VALIDATION_OUTPUT out_type) {
         Mat2D Xv = X.bottomRows(X.rows()-1);
         Mat2D Yv = Y.bottomRows(Y.rows()-1);
-       
+
         Mat2D SSEv = Mat2D::Zero(Y.cols(), this->A);
 
         PLS_Model plsm_v;
@@ -149,8 +171,8 @@ class PLS_Model {
                 // tally the squared errors
                 SSEv.col(j-1) += res.cwiseProduct(res).transpose();
             }
-            Xv.row(i) = X.row(i); 
-            Yv.row(i) = Y.row(i); 
+            Xv.row(i) = X.row(i);
+            Yv.row(i) = Y.row(i);
         }
         if ( out_type == PRESS ) {
             return SSEv;
@@ -161,10 +183,10 @@ class PLS_Model {
     }
 
 
-    std::vector<Mat2D> _loo_cv_error_matrix(const Mat2D& X, const Mat2D& Y) { 
+    std::vector<Mat2D> _loo_cv_error_matrix(const Mat2D& X, const Mat2D& Y) {
         Mat2D Xv = X.bottomRows(X.rows()-1);
         Mat2D Yv = Y.bottomRows(Y.rows()-1);
-       
+
         // vector of error matrices(rows=Y.rows(), cols=Y.cols())
         // col = component #, row = obs #, tier = Y category
         std::vector<Mat2D> Ev(Y.cols(), Mat2D::Zero(X.rows(), this->A));
@@ -180,15 +202,15 @@ class PLS_Model {
                 for (int k = 0; k < res.size(); k++) Ev[k](i,j-1) = res(k);
             }
             if (i < Xv.rows()) {
-                Xv.row(i) = X.row(i); 
-                Yv.row(i) = Y.row(i); 
+                Xv.row(i) = X.row(i);
+                Yv.row(i) = Y.row(i);
             }
         }
         return Ev;
-    }   
+    }
 
 
-    std::vector<Mat2D> _new_data_cv_error_matrix(const Mat2D& X_new, const Mat2D& Y_new) { 
+    std::vector<Mat2D> _new_data_cv_error_matrix(const Mat2D& X_new, const Mat2D& Y_new) {
         // vector of error matrices(rows=Y.rows(), cols=Y.cols())
         // col = component #, row = obs #, tier = Y category
         std::vector<Mat2D> Ev(Y_new.cols(), Mat2D::Zero(X_new.rows(), this->A));
@@ -204,7 +226,7 @@ class PLS_Model {
 
     // if val_method is LOO, X and Y should be original data
     // if val_method is NEW_DATA, X and Y should be observations not included in the original model
-    Rowi optimal_num_components(const Mat2D& X, const Mat2D& Y, VALIDATION_METHOD val_method) { 
+    Rowi optimal_num_components(const Mat2D& X, const Mat2D& Y, VALIDATION_METHOD val_method) {
         // col = component #, row = obs #, tier = Y category
 
         std::vector<Mat2D> errors;
@@ -218,16 +240,16 @@ class PLS_Model {
         Rowi min_press_idx = Rowi::Zero(Y.cols());
         Row  min_press_val(Y.cols());
         Rowi best_comp(Y.cols());
-        
+
         // Determine PRESS values
         for (unsigned int i=0; i<errors.size(); i++) {    // for each Y category
-            for (int j=0; j<errors[i].rows(); j++) {      // for each observation 
+            for (int j=0; j<errors[i].rows(); j++) {      // for each observation
                 for (int k=0; k<errors[i].cols(); k++) {  // for each component
-                    press(i,k) += pow(errors[i](j,k), 2); 
+                    press(i,k) += pow(errors[i](j,k), 2);
                 }
             }
         }
-        
+
         min_press_val = press.col(0);
         // Find the component number that minimizes PRESS for each Y category
         for (int i=0; i<press.rows(); i++) {              // for each Y category
