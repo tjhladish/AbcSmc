84c575e3f59eada3cbc2012d2a97513b6f456776
Carl A. B. Pearson <carl.pearson@lshtm.ac.uk>
2023-02-02 19:23:34 +0000

    fix examples/example.h

--- examples/examples.h
+++ examples/examples.h
@@ -56,24 +56,29 @@ CLIArgs parse_args(std::string cmd, int argc, char* argv[]) {
 };
 
 template<typename ABC>
+void abc_inner(ABC * abc, CLIArgs& args, const gsl_rng * RNG, size_t buffer_size) {
+
+    if (args.process_db) {
+        gsl_rng_set(RNG, time(NULL) * getpid()); // seed the rng using sys time and the process id
+        abc->process_database(RNG);
+    } 
+
+    if (args.simulate_db) {
+        abc->simulate_next_particles(buffer_size);
+    }
+
+};
+
+template<typename ABC>
 void abc_loop(ABC * abc, CLIArgs& args, const gsl_rng * RNG) {
-    size_t set_count = abc->get_smc_iterations();
 
-    for (size_t i = 0; i < set_count; ++i) {
-        auto buffer_size = args.buffer_size;
-
-        if (args.do_all) {
-            buffer_size = abc->get_num_particles(i, QUIET);
+    if (args.do_all) {
+        size_t set_count = abc->get_smc_iterations();
+        for (size_t i = 0; i < set_count; ++i) {
+            abc_inner(abc, args, RNG, abc->get_num_particles(i, QUIET));
         }
-
-        if (args.process_db) {
-            gsl_rng_set(RNG, time(NULL) * getpid()); // seed the rng using sys time and the process id
-            abc->process_database(RNG);
-        } 
-
-        if (args.simulate_db) {
-            abc->simulate_next_particles(buffer_size);
-        }
+    } else {
+        abc_inner(abc, args, RNG, args.buffer_size);
     }
 
     if (args.do_all) {
