0e71904a33725c478878730ae31462fccc426c13
Carl A. B. Pearson <carl.pearson@lshtm.ac.uk>
2023-02-14 06:55:43 +0000

    more db view work

--- retrofitdb.sql
+++ retrofitdb.sql
@@ -1,5 +1,13 @@
+/* SQLite supports VIEWs - these are essentially predefined queries, that are dynamically
+*  evaluated, and function as-if they were TABLEs.
+*
+*  For `AbcSmc` SQLite-based storage, we define several views which encapsulate queries that
+*  represent common operations as part of the analysis flow.
+*/
 
-/* add the "smc set" view */
+/* ================== SMC Set Level Operations & Posterior Queries ===================== */
+
+/* add the "smc set summary" view */
 CREATE VIEW IF NOT EXISTS smc_summary AS
     SELECT smcSet, MIN(serial) AS start_serial, MAX(serial) AS end_serial,
         COUNT(*) AS total, SUM(CASE WHEN status == 'D' THEN 1 ELSE 0 END) AS run,
@@ -8,6 +16,8 @@ CREATE VIEW IF NOT EXISTS smc_summary AS
     GROUP BY smcSet ORDER BY smcSet;
 
 /* smc sets where total == run AND posterior > 0 are considered complete */
+/* smc sets where total == run AND posterior == 0 are considered TODO */
+/* smc sets where total > run => still to be simulated */
 /* TODO: if configuration data were available in db, could assess if the
 *  "right" number of jobs were run, and if the "right" number of jobs have
 *  posterior > -1
@@ -16,7 +26,9 @@ CREATE VIEW IF NOT EXISTS smc_summary AS
 /* add the "complete posterior" smc waves view */
 CREATE VIEW IF NOT EXISTS post_done AS
     SELECT serial, smcSet, posterior
-    FROM job WHERE posterior != -1 AND status == 'D'
+    FROM job
+    JOIN (SELECT smcSet FROM smc_summary WHERE total == run AND posterior != 0)
+    USING (smcSet)
     ORDER BY smcSet, posterior;
 
 /* add the "last complete posterior" view */
@@ -26,8 +38,21 @@ CREATE VIEW IF NOT EXISTS post_last AS
     ) USING (smcSet)
     ORDER BY posterior;
 
+/* add the "still to process" view */
+/* NB: not aware of uses were there are multiple done-but-not processed waves, but
+*  can imagine a situation where that might be useful? Hence still introducing
+* the all (post_todo) vs one (post_next) distinction
+*/
 CREATE VIEW IF NOT EXISTS post_todo AS
-    SELECT * FROM post_all JOIN (
-        SELECT MAX(smcSet) AS smcSet FROM post_all
+    SELECT serial, smcSet
+    FROM job
+    JOIN (SELECT smcSet FROM smc_summary WHERE total == run AND posterior == 0)
+    USING (smcSet)
+    ORDER BY smcSet, posterior;
+
+/* add the "next to process" view */
+CREATE VIEW IF NOT EXISTS post_next AS
+    SELECT * FROM post_todo JOIN (
+        SELECT MIN(smcSet) AS smcSet FROM post_todo
     ) USING (smcSet)
     ORDER BY posterior;
