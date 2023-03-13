0bc2b6989389ce2847f007e0baaebf7443d59c30
Carl A. B. Pearson <carl.pearson@lshtm.ac.uk>
2023-03-13 13:20:43 +0000

    reflecting move of items to PLS

--- AbcUtil.h
+++ AbcUtil.h
@@ -52,16 +52,6 @@ namespace ABC {
 
     //int _sgn(float_type val) { return (0 < val) - (val < 0); }
 
-    Mat2D colwise_z_scores( const Mat2D & mat, const Row & mean, const Row & stdev);
-    Mat2D colwise_z_scores(const Mat2D & mat) {
-        Row means = mat.colwise().mean();
-        Row stdev = colwise_stdev(mat, means);
-        return colwise_z_scores(mat, means, stdev);
-    };
-    Row z_scores(const Row & metobs, const Row & means, const Row & stdevs);
-
-    float_type mean(const Col & data);
-
     float_type median(const Col & data);
 
     float_type quantile(const Col & data, double q);
@@ -78,9 +68,7 @@ namespace ABC {
 
     float_type optimize_box_cox(const Col & data, const float lambda_min, const float lambda_max, const float step);
 
-    float_type optimize_box_cox(const Col & data)  {
-        return optimize_box_cox(data, -5, 5, 0.1);
-    }
+    float_type optimize_box_cox(const Col & data);
 
     template <typename T>
     inline void cerr_vector(std::vector<T> & my_vector, std::string sep = " ") {
