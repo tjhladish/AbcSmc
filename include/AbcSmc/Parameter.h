feaea55905aa0c47a807f3b6e53a883111077672
Carl A. B. Pearson <carl.pearson@lshtm.ac.uk>
2023-03-17 15:12:19 +0000

    WIP on ParXform

--- include/AbcSmc/Parameter.h
+++ include/AbcSmc/Parameter.h
@@ -38,41 +38,6 @@ using std::is_floating_point_v;
 //  - have transformations managed by the ABCSMC object?
 
 namespace ABC {
-
-    // TODO relocate this
-    // this defines shift/scale transformation.
-    //
-    // a = sum of transformed scale addends
-    // b = prod of transformed scale factors
-    // c = sum of untransformed scale addends
-    // d = prod of untransformed scale factors
-    // u = arbitrary untransform function
-    // x = value on the transformed scale (i.e. the fitting scale)
-    //
-    // transform(x) = (u((x + a) * b) + c) * d (i.e. value on the model/natural scale)
-    struct ParXform {
-
-        ParXform(
-            const float_type tp = 0.0, const float_type up = 0.0,
-            const float_type tt = 1.0, const float_type ut = 1.0
-        ) : tplus(tp), uplus(up), ttimes(tt), utimes(ut) {}
-
-        template <typename T> 
-        ParXform(const T & pre_shift, const T & post_shift, const T & pre_scale, const T & post_scale) :
-        ParXform(
-            std::accumulate(pre_shift.begin(), pre_shift.end(), 0.0),
-            std::accumulate(post_shift.begin(), post_shift.end(), 0.0),
-            std::accumulate(pre_scale.begin(), pre_scale.end(), 1.0, std::multiplies<float_type>()),
-            std::accumulate(post_scale.begin(), post_scale.end(), 1.0, std::multiplies<float_type>())
-        ) { }
-
-        const float_type tplus, uplus, ttimes, utimes;
-
-        float_type transform(const float_type & pval, float_type (*u)(const float_type &)) const {
-            return (u((pval + tplus)*ttimes) + uplus)*utimes;
-        }
-
-    };
     
     class Parameter {
         public:
