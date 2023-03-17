c372133d0c6ab3df804d1bec2ae9fd105d561ecf
Carl A. B. Pearson <carl.pearson@lshtm.ac.uk>
2023-03-17 15:12:19 +0000

    WIP on ParXform

--- include/AbcSmc/ParXform.h
+++ include/AbcSmc/ParXform.h
@@ -1,49 +1,62 @@
 #ifndef ABCSMC_PARXFORM_H
 #define ABCSMC_PARXFORM_H
 
-#include <AbcSmc/Parameter.h>
 #include <vector>
 
 namespace ABC {
 
-// this defines shift/scale transformation.
+// this defines shift/scale transformation, from _fitting_ space (a/k/a transformed) to _model_ space (a/k/a untransformed).
 //
 // a = sum of transformed scale addends
 // b = prod of transformed scale factors
 // c = sum of untransformed scale addends
 // d = prod of untransformed scale factors
 // u = arbitrary untransform function
-// x = value on the transformed scale (i.e. the fitting scale)
+// x = value on the _fitting_ scale
+// x' = value on the _model_ scale
 //
-// transform(x) = (u((x + a) * b) + c) * d (i.e. value on the model/natural scale)
+// x' = (u((x + a) * b) + c) * d (i.e. value on the model/natural scale)
+//
+// When transforming, expects to be passed a vector of _fitting_ space values.
+// These combined according to specification for addends and factors to produce (a,b,c,d).
 struct ParXform {
 
     ParXform(
-        const float_type tp = 0.0, const float_type up = 0.0,
-        const float_type tt = 1.0, const float_type ut = 1.0
-    ) : tplus(tp), uplus(up), ttimes(tt), utimes(ut) {}
+        const std::vector<size_t> & fitting_space_add_idx,
+        const std::vector<size_t> & fitting_space_mul_idx,
+        const std::vector<size_t> & model_space_add_idx,
+        const std::vector<size_t> & model_space_mul_idx,
+    ) : _idx_fitting_space_addends(fitting_space_add_idx),
+        _idx_fitting_space_factors(fitting_space_mul_idx),
+        _idx_model_space_addends(model_space_add_idx),
+        _idx_model_space_factors(model_space_mul_idx) {    
+    }
 
-    template <typename T> 
-    ParXform(const T & pre_shift, const T & post_shift, const T & pre_scale, const T & post_scale) :
-    ParXform(
-        std::accumulate(pre_shift.begin(), pre_shift.end(), 0.0),
-        std::accumulate(post_shift.begin(), post_shift.end(), 0.0),
-        std::accumulate(pre_scale.begin(), pre_scale.end(), 1.0, std::multiplies<float_type>()),
-        std::accumulate(post_scale.begin(), post_scale.end(), 1.0, std::multiplies<float_type>())
-    ) { }
-
-    const float_type tplus, uplus, ttimes, utimes;
-
-    float_type transform(const float_type & pval, float_type (*u)(const float_type &)) const {
+    // T is some sort of random access container of float_type
+    // in AbcSmc, `Row`s of parameters, generally - but could be any container
+    // this enables testing with simpler containers
+    template <typename T>
+    float_type transform(
+        const float_type & pval, const T & fitting_space_values
+    ) const {
+        float_type tplus = 0.0, ttimes = 1.0, uplus = 0.0, utimes = 1.0;
+        for (auto i : _idx_fitting_space_addends) { tplus += fitting_space_values[i]; }
+        for (auto i : _idx_fitting_space_factors) { ttimes *= fitting_space_values[i]; }
+        for (auto i : _idx_model_space_addends) { uplus += fitting_space_values[i]; }
+        for (auto i : _idx_model_space_factors) { utimes *= fitting_space_values[i]; }
         return (u((pval + tplus)*ttimes) + uplus)*utimes;
     }
 
     private:
-        std::vector<size_t> _idx_fitting_space_addends;
-        std::vector<size_t> _idx_fitting_space_factors;
-        std::vector<size_t> _idx_model_space_addends;
-        std::vector<size_t> _idx_model_space_factors;
+        const std::vector<size_t> _idx_fitting_space_addends;
+        const std::vector<size_t> _idx_fitting_space_factors;
+        const std::vector<size_t> _idx_model_space_addends;
+        const std::vector<size_t> _idx_model_space_factors;
 
+};
+
+struct ParRescale {
+    
 };
 
 }
