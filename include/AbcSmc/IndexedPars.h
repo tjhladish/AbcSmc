e34ebc97c4f3668bb697d1d7cfcc43d2f41fdbe2
Carl A. B. Pearson <carl.pearson@lshtm.ac.uk>
2023-03-18 21:13:12 +0000

    compiles param separation

--- include/AbcSmc/IndexedPars.h
+++ include/AbcSmc/IndexedPars.h
@@ -2,52 +2,56 @@
 #ifndef ABCSMC_INDEXEDPARS_H
 #define ABCSMC_INDEXEDPARS_H
 
-#include <AbcSmc/Parameters.h>
+#include <AbcSmc/Parameter.h>
 #include <iostream>
 
 namespace ABC {
 
 // A Prior isa Parameter that does random sampling, and has meaningful mean, sd, noise, and likelihood
 // still an Abstract Base Class however!
-class IndexedPar : Parameter, public virtual TParameter<size_t, Parameter> {
-    public:
-        IndexedPar(
-            const std::string & s, const std::string & ss,
-            const size_t maxIdx
-        ) : Parameter(s, ss), max_index(maxIdx) {}
+struct IndexedPar : public Parameter {
+    IndexedPar(
+        const std::string & s, const std::string & ss,
+        const size_t maxIdx
+    ) : Parameter(s, ss, maxIdx) {}
 
     float_type likelihood(const float_type pval) const override {
         std::cerr << "ERROR: it is an error to ask for likelihood from an IndexedPar; attempted on " << get_name() << std::endl;
         exit(-1);
     }
 
+    float_type recast(const float_type pval) const override {
+        std::cerr << "ERROR: it is an error to attempt to recast an IndexedPar; attempted on " << get_name() << std::endl;
+        exit(-1);
+    }
+
 };
 
-class PseudoPar : IndexedPar {
-    public:
-        PseudoPar(
-            const std::string & s, const std::string & ss,
-            const std::vector<float_type> & vals
-        ) : IndexedPar(s, ss, vals.size()-1), states(vals) {}
+struct PseudoPar : public IndexedPar {
+    PseudoPar(
+        const std::string & s, const std::string & ss,
+        const std::vector<float_type> & vals
+    ) : IndexedPar(s, ss, vals.size()-1), states(vals) {}
 
-        // TODO support other constructors?
+    // TODO support other constructors?
 
-        float_type sample(PRNG & prng) const override { return states[rng.pseudo(this)]; }
+    float_type sample(PRNG & prng) const override { return states[prng.pseudo(this)]; }
 
     protected:
         const std::vector<float_type> states;
 
 };
 
-class PosteriorPar : IndexedPar {
-    PseudoPar(
+struct PosteriorPar : public IndexedPar {
+    PosteriorPar(
         const std::string & s, const std::string & ss,
         const size_t maxIdx
     ) : IndexedPar(s, ss, maxIdx) {}
 
     float_type sample(PRNG & rng) const override { return static_cast<float_type>(rng.posterior()); }
+    bool isPosterior() const override { return true; }
 };
 
 }
 
-#endif // ABCSMC_PRIORS_H
+#endif // ABCSMC_INDEXEDPARS_H
--- include/AbcSmc/Priors.h
+++ include/AbcSmc/Priors.h
@@ -2,82 +2,109 @@
 #ifndef ABCSMC_PRIORS_H
 #define ABCSMC_PRIORS_H
 
-#include <AbcSmc/Parameters.h>
+#include <AbcSmc/Parameter.h>
+#include <gsl/gsl_rng.h>
+#include <gsl/gsl_randist.h>
 
 namespace ABC {
 
 // A Prior isa Parameter that does random sampling, and has meaningful mean, sd, noise, and likelihood
 // still an Abstract Base Class however!
-class Prior : Parameter {
-    public:
-        Prior(
-            const std::string & s, const std::string & ss,
-            const float_type mv, const float_type sv
-        ) : Parameter(s, ss), meanval(mv), sdval(sv) {}
+struct Prior : public Parameter {
+    Prior(
+        const std::string & s, const std::string & ss,
+        const float_type mv, const float_type sv
+    ) : Parameter(s, ss), meanval(mv), sdval(sv) {}
 
-        float_type noise(
-            PRNG & prng, const float_type mu, const float_type sigma,
-            const size_t MAX_ATTEMPTS = 1000
-        ) const override {
-            size_t attempts = 1;
-            auto dev = trynoise(prng, mu, sigma);
-            while(!valid(dev) and (attempts++ < MAX_ATTEMPTS)) { dev = trynoise(prng, mu, sigma); }
-            if (!valid(dev)) { 
-                std::cerr << "ERROR: failed to draw valid noise from prior " << get_name() << " - returning mean value." std::endl;
-                return get_mean();
-            } else {
-                return dev;
-            }
+    float_type noise(
+        const gsl_rng * rng, const float_type mu, const float_type sigma,
+        const size_t MAX_ATTEMPTS = 1000
+    ) const override {
+        size_t attempts = 1;
+        auto dev = trynoise(rng, mu, sigma);
+        while(!valid(dev) and (attempts++ < MAX_ATTEMPTS)) { dev = trynoise(rng, mu, sigma); }
+        if (!valid(dev)) { 
+            std::cerr << "ERROR: failed to draw valid noise from prior " << get_name() << " - returning mean value." << std::endl;
+            return get_mean();
+        } else {
             return dev;
-        };
+        }
+        return dev;
+    };
 
-        float_type get_mean() const override { return meanval; }
-        float_type get_sd() const override { return sdval; }
+    float_type get_mean() const override { return meanval; }
+    float_type get_sd() const override { return sdval; }
 
     protected:
         const float_type meanval, sdval;
-        float_type trynoise(const gsl_rng * prng, const float_type mu, const float_type sigma) const {
-            return recast(gsl_ran_gaussian(prng, sigma) + mu);
+        float_type trynoise(const gsl_rng * rng, const float_type mu, const float_type sigma) const {
+            return recast(gsl_ran_gaussian(rng, sigma) + mu);
         };
 
 };
 
-class GaussianPrior : Prior, public virtual TParameter<float_type, Prior> {
-    public:
-        GaussianPrior(
-            const std::string & nm, const std::string & snm,
-            const float_type mn, const float_type sd
-        ) : Prior(nm, snm, mn, sd) {}
+struct GaussianPrior : public Prior {
+    GaussianPrior(
+        const std::string & nm, const std::string & snm,
+        const float_type mn, const float_type sd
+    ) : Prior(nm, snm, mn, sd) {}
+    
+    float_type sample(PRNG & prng) const override { return gsl_ran_gaussian(prng.rng(), sdval) + meanval; };
+
+    float_type likelihood(const float_type pval) const override {
+        return gsl_ran_gaussian_pdf(pval - meanval, sdval);
+    };
+
+    float_type recast(const float_type pval) const override { return pval; }
 };
 
-class DiscreteUniformPrior : Prior, public virtual TParameter<long, Prior> {
-    public:
-        DiscreteUniformPrior(
-            const std::string & nm, const std::string & snm,
-            const long min, const long min
-        ) : Prior(
-            nm, snm,
-            static_cast<float_type>(max + min) / 2.0, static_cast<float_type>(max - min) / sqrt(12.0)
-        ), minval(min), maxval(max) {
-            assert(min < max);
-        }
-    
+struct DiscreteUniformPrior : public Prior {
+    DiscreteUniformPrior(
+        const std::string & nm, const std::string & snm,
+        const long min, const long max
+    ) : Prior(
+        nm, snm,
+        static_cast<float_type>(max + min) / 2.0, static_cast<float_type>(max - min) / sqrt(12.0)
+    ), minval(min), maxval(max) {
+        assert(min < max);
+    }
+
+    float_type sample(PRNG & prng) const override {
+        return static_cast<float_type>(gsl_rng_uniform_int(prng.rng(), maxval - minval + 1) + minval);
+    };
+
+    float_type likelihood(const float_type pval) const override {
+        return ((pval == recast(pval)) and (minval <= pval) and (pval <= maxval)) ? 1.0 / (maxval - minval + 1) : 0.0;
+    };
+
+    float_type recast(const float_type pval) const override { return static_cast<float_type>(std::round(pval)); }
+
     private:
         const long minval, maxval;
 };
 
-class ContinuousUniformPrior : Prior, public virtual TParameter<float_type, Prior> {
-    public:
-        ContinuousUniformPrior(
-            const std::string & nm, const std::string & snm,
-            const float_type min, const float_type min
-        ) : Prior(
-            nm, snm,
-            static_cast<float_type>(max + min) / 2.0,
-            static_cast<float_type>(max - min) / sqrt(12.0)
-        ), minval(min), maxval(max) {
-            assert(min < max);
-        }
+struct ContinuousUniformPrior : public Prior {
+    ContinuousUniformPrior(
+        const std::string & nm, const std::string & snm,
+        const float_type min, const float_type max
+    ) : Prior(
+        nm, snm,
+        static_cast<float_type>(max + min) / 2.0,
+        static_cast<float_type>(max - min) / sqrt(12.0)
+    ), minval(min), maxval(max) {
+        assert(min < max);
+    }
+
+    float_type sample(PRNG & prng) const override {
+        return gsl_rng_uniform(prng.rng())*(maxval-minval) + minval;
+    };
+
+    float_type likelihood(const float_type pval) const override {
+        return ((minval <= pval) and (pval <= maxval)) ? 1.0 / (maxval - minval) : 0.0;
+    };
+
+    float_type recast(const float_type pval) const override { return pval; }
+
     private:
         const float_type minval, maxval;
 };
