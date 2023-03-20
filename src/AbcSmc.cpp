4eef36b29161dfd676d8fb011fcde73d24d87419
Carl A. B. Pearson <carl.pearson@lshtm.ac.uk>
2023-03-20 20:16:43 +0000

    have parse_iterations work with pointers

--- src/AbcSmc.cpp
+++ src/AbcSmc.cpp
@@ -53,10 +53,10 @@ vector<T> as_vector(const Json::Value & val) {
 
 void parse_iterations(
     const Json::Value & par, const size_t pseudosize,
-    size_t & iterations,
-    float_type & training_frac,
-    vector<size_t> & set_sizes,
-    vector<size_t> & pred_prior_sizes
+    size_t * iterations,
+    float_type * training_frac,
+    vector<size_t> * set_sizes,
+    vector<size_t> * pred_prior_sizes
 ) {
     if (pseudosize != 0) { // definitely in projection mode; ignore other parameters
         if (par.get("smc_iterations", 1).asInt() != 1) { // ignoring OR erroring if specified != 1
@@ -75,8 +75,8 @@ void parse_iterations(
         if (par.isMember("predictive_prior_fraction") or par.isMember("predictive_prior_size")) {
             std::cerr << "WARNING: ignoring `predictive_prior_*` options in projection mode." << std::endl;
         }
-        iterations = 1;
-        set_sizes = { pseudosize };
+        *iterations = 1;
+        *set_sizes = { pseudosize };
     } else { // some priors => fitting mode => predictive prior fractions
         // pred_prior_sizes is a series of sizes, but may be passed as EITHER fractions OR sizes.
         // both / neither is an error
@@ -89,13 +89,13 @@ void parse_iterations(
         }
 
         // if doing iterations, may have a training fraction; defaults to 0.5
-        training_frac = par.get("pls_training_fraction", 0.5).as<float_type>();
-        if (training_frac <= 0 or 1 <= training_frac) {
+        *training_frac = par.get("pls_training_fraction", 0.5).as<float_type>();
+        if (*training_frac <= 0 or 1 <= *training_frac) {
             std::cerr << "Error: pls_training_fraction must be in (0, 1)." << std::endl;
             exit(1);
         }
 
-        set_sizes = as_vector<size_t>(par["num_samples"]);
+        *set_sizes = as_vector<size_t>(par["num_samples"]);
 
         if (par.isMember("predictive_prior_fraction")) {
             auto ppfs = as_vector<float_type>(par["predictive_prior_fraction"]);
@@ -103,34 +103,34 @@ void parse_iterations(
                 cerr << "Error: `predictive_prior_fraction`s must be in (0, 1]" << endl;
                 exit(1);
             }
-            auto set_sizes_copy = set_sizes;
+            auto set_sizes_copy = *set_sizes;
             const int max_set = max(ppfs.size(), set_sizes_copy.size());
             ppfs.resize(max_set, ppfs.back());
             set_sizes_copy.resize(max_set, set_sizes_copy.back());
-            pred_prior_sizes.resize(max_set);
-            for (size_t i = 0; i < max_set; ++i) { pred_prior_sizes[i] = round(ppfs[i] * set_sizes_copy[i]); }
+            pred_prior_sizes->resize(max_set);
+            for (size_t i = 0; i < max_set; ++i) { (*pred_prior_sizes)[i] = round(ppfs[i] * set_sizes_copy[i]); }
         } else { // if (par.isMember("predictive_prior_size")) {
-            pred_prior_sizes = as_vector<size_t>(par["predictive_prior_size"]);
-            const size_t max_set = max(pred_prior_sizes.size(), set_sizes.size());
+            *pred_prior_sizes = as_vector<size_t>(par["predictive_prior_size"]);
+            const size_t max_set = max(pred_prior_sizes->size(), set_sizes->size());
             size_t i = 0;
-            for (; i < max_set; ++i) { if (pred_prior_sizes[i] > set_sizes[i]) {
+            for (; i < max_set; ++i) { if (pred_prior_sizes->at(i) > set_sizes->at(i)) {
                 cerr << "Error: requested predictive prior size > SMC set size at: " << i << endl;
                 exit(1);
             } }
             // now either done with both, or one has more to do, so if there *are* more...
             // EITHER more pred prior
-            for (; i < pred_prior_sizes.size(); ++i) { if (pred_prior_sizes[i] > set_sizes.back()) {
+            for (; i < pred_prior_sizes->size(); ++i) { if (pred_prior_sizes->at(i) > set_sizes->back()) {
                 cerr << "Error: requested predictive prior size > SMC set size at: " << i << endl;
                 exit(1);
             } }
             // OR more set sizes
-            for (; i < set_sizes.size(); ++i) { if (pred_prior_sizes.back() > set_sizes[i]) {
+            for (; i < set_sizes->size(); ++i) { if (pred_prior_sizes->back() > set_sizes->at(i)) {
                 cerr << "Error: requested predictive prior size > SMC set size at: " << i << endl;
                 exit(1);
             } }
         }
 
-        iterations = par.get("smc_iterations", max(set_sizes.size(), pred_prior_sizes.size())).asUInt64();
+        *iterations = par.get("smc_iterations", max(set_sizes->size(), pred_prior_sizes->size())).asUInt64();
 
     }
   
@@ -158,7 +158,7 @@ void parse_transform(
     ParRescale ** pscale,
     ParXform ** pxform,
     transformer ** _untransform_func,
-    const std::map<const std::string, const size_t> par_name_idx
+    const std::map<const std::string, const size_t> & par_name_idx
 ) {
     if (mparu.type() == Json::ValueType::stringValue) {
         string ttype_str = mparu.asString();
@@ -382,7 +382,7 @@ bool AbcSmc::parse_config(const string & conf_filename) {
 
     for (const Json::Value & mmet : par["metrics"]) { add_next_metric(parse_metric(mmet)); }
 
-    parse_iterations(par, pseudosize, _num_smc_sets, _pls_training_fraction, _smc_set_sizes, _predictive_prior_sizes);
+    parse_iterations(par, pseudosize, &_num_smc_sets, &_pls_training_fraction, &_smc_set_sizes, &_predictive_prior_sizes);
 
     // TODO these should be mutually exclusive options
     std::string executable = par.get("executable", "").asString();
